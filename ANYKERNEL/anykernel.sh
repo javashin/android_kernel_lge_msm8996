f63c02caa1edf7adcb3d87ea60bdffa8d31e7ce0
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2020-06-19 10:44:41 -0400

    • Fix Compiling with Google Android clang-master. • Enable Adreno Idler. • Enable State-Notifier Driver. • Added Dynamic Fsync 2.0 • GPU-Enable-19MHz-idle-frequency. • FIX soc/qcom: mpm-of: Remove __init declaration from mpm_irq_domain_size()  - mpm_irq_domain_size is wrong. No More Mismatch * • Add JavaShin-X CustoKernal Compile-System. • Add jsx_custokernal_h872_defconfig. • Anykernel v3.

---
+++ ANYKERNEL/anykernel.sh
@@ -0,0 +1,75 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string="Custo-Kernal-JavaShin-X-H872 Kernel For LG G6 T-Mobile"
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=h872
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+#backup_file init.rc;
+#replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+# init.tuna.rc
+#backup_file init.tuna.rc;
+#insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.lucye" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+#append_file init.tuna.rc "bootscript" init.tuna;
+
+# fstab.lucye
+#backup_file fstab.lucye;
+#patch_fstab fstab.lucye /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+#patch_fstab fstab.lucye /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+#patch_fstab fstab.lucye /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+#append_file fstab.lucye "usbdisk" fstab;
+
+# end ramdisk changes
+
+ui_print " "
+ui_print "Device: LG G6 (H872)"
+ui_print "Kernel Name: Custo-Kernal-JavaShin-X"
+ui_print "Build User: root"
+ui_print "KVer: 3.18.140-jsX-perf_r1 JavaShin-X-H872 SMP PREEMPT aarch64"
+ui_print "June 2020 "
+ui_print "Toolchain Info:"
+ui_print "Android (6573524 based on r383902b) clang version 11.0.2 (https://android.googlesource.com/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79)"
+
+ui_print " "
+
+write_boot;
+## end install
+
---
+++ Compile-h872.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+clear 
+
+
+echo "KERNAL KERNAL KERNAL KERNAL KARNAL KARMA KARMA KARMA"
+date
+echo "Start Compilation ^^^^^@^^^^  Time"
+echo "Set TurboBoost For Faster Android Kernal Compilation"
+/usr/bin/cpupower frequency-set -g performance
+/usr/sbin/x86_energy_perf_policy performance
+#printf "1"  > /sys/devices/system/cpu/cpufreq/boost
+
+
+
+
+
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "Preparing"
+sleep 1
+
+rm -rf OUT
+export ARCH=arm64  
+export SUBARCH=arm 
+mkdir OUT 
+
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "Configuring"
+sleep 1
+
+PATH="/mnt/android-B/clang-master/bin:/mnt/android-B/aarch64-linux-android-4.9/bin:${PATH}" make O=OUT ARCH=arm64 SUBARCH=arm CC=clang DTC_EXT=dtc SELINUX_DEFCONFIG=selinux_defconfig CONFIG_ADRENO_IDLER=y CONFIG_STATE_NOTIFIER=y CONFIG_DYNAMIC_FSYNC=y CONFIG_BUILD_ARM64_DT_OVERLAY=y CLANG_TRIPLE=aarch64-linux-android- CROSS_COMPILE=aarch64-linux-android- CONFIG_NO_ERROR_ON_MISMATCH=y lineageos_h872_defconfig
+#cp /H872-OPTIMORE OUT/.config
+PATH="/mnt/android-B/clang-master/bin:/mnt/android-B/aarch64-linux-android-4.9/bin:${PATH}" make O=OUT ARCH=arm64 SUBARCH=arm CC=clang DTC_EXT=dtc SELINUX_DEFCONFIG=selinux_defconfig CONFIG_ADRENO_IDLER=y CONFIG_STATE_NOTIFIER=y CONFIG_DYNAMIC_FSYNC=y CONFIG_BUILD_ARM64_DT_OVERLAY=y CLANG_TRIPLE=aarch64-linux-android- CROSS_COMPILE=aarch64-linux-android- CONFIG_NO_ERROR_ON_MISMATCH=y oldconfig
+PATH="/mnt/android-B/clang-master/bin:/mnt/android-B/aarch64-linux-android-4.9/bin:${PATH}" make O=OUT ARCH=arm64 SUBARCH=arm CC=clang DTC_EXT=dtc SELINUX_DEFCONFIG=selinux_defconfig CONFIG_ADRENO_IDLER=y CONFIG_STATE_NOTIFIER=y CONFIG_DYNAMIC_FSYNC=y CONFIG_BUILD_ARM64_DT_OVERLAY=y CLANG_TRIPLE=aarch64-linux-android- CROSS_COMPILE=aarch64-linux-android- CONFIG_NO_ERROR_ON_MISMATCH=y nconfig
+
+
+
+
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "Compiling"
+sleep 1
+
+#export KCFLAGS+="-Wno-enum-conversion -Wno-uninitialized -Wno-logical-not-parentheses \
+#-Wno-parentheses -Wno-incompatible-pointer-types -fno-stack-protector \
+#-fno-merge-constants -Wno-ignored-optimization-argument -Wno-error \
+#-Wno-tautological-constant-out-of-range-compare -Wno-xor-used-as-pow -Wno-fortify-source -O3 -fno-stack-protector"
+
+KBUILD_CFLAGS+="-mcpu=kyro -mcpu=cortex-a72.cortex-a53 -march=armv8-a+fp+simd+crc+crypto -march=armv8-a+fp+simd+crc+crypto"
+
+PATH="/mnt/android-B/clang-master/bin:/mnt/android-B/aarch64-linux-android-4.9/bin:${PATH}" make -j$(nproc --all) O=OUT ARCH=arm64 SUBARCH=arm CC=clang DTC_EXT=dtc SELINUX_DEFCONFIG=selinux_defconfig CONFIG_BUILD_ARM64_DT_OVERLAY=y KCFLAGS+="-Wno-error -O3 -fno-stack-protector -march=armv8-a+fp+simd+crc+crypto"  CLANG_TRIPLE=aarch64-linux-android- CROSS_COMPILE=aarch64-linux-android- CONFIG_NO_ERROR_ON_MISMATCH=y CONFIG_ADRENO_IDLER=y CONFIG_STATE_NOTIFIER=y CONFIG_DYNAMIC_FSYNC=y CONFIG_DEBUG_SECTION_MISMATCH=y V=0
+
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "DONE"
+echo "Set Normal Speed To Cooldown CPU After Compilation"
+/usr/bin/cpupower frequency-set -g schedutil
+/usr/sbin/x86_energy_perf_policy normal
+#printf "0"  > /sys/devices/system/cpu/cpufreq/boost
+
+
+
+rm ANYKERNEL/Image.gz-dtb
+cp OUT/arch/arm64/boot/Image.gz-dtb ANYKERNEL/
+cd ANYKERNEL/
+ls -lash Image.gz-dtb
+rm *.zip
+zip -r9 H872-jsX-CustoKernal-19-JUNE-2020-TESTBUILD.zip * -x .git README.md *placeholder
+ls -lash H872-jsX-CustoKernal-19-JUNE-2020-TESTBUILD.zip
+cp H872-jsX-CustoKernal-19-JUNE-2020-TESTBUILD.zip /home/javashin/Desktop/
+#adb push UPDATE-H872-AnyKernel3-24-APRIL-2020.zip /sdcard
+
+echo "YAY"
+echo "KERNAL KERNAL KERNAL KERNAL KARNAL KARMA KARMA KARMA"
+echo "Done By JavaShin-X Carlos Jimenez 2020."
